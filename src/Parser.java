952ed419895d7d6be3d102060b6f2c021a3f9923
Manuel <manu.valladolid@gmail.com>
2012-12-24 12:17:50 +0100

    Fixed Parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,5 +1,9 @@
 
 import java.io.*;
+import java.util.Scanner;
+import java.io.File;
+
+
 
 class Transicion
 {
@@ -9,82 +13,116 @@ class Transicion
 	public int nuevoValorMaquina; // p
 	public int nuevoValorCinta; // f
 	public int direccion; // m
+	
+	Transicion()
+	{
+		valorMaquina = 0;
+		valorCinta = 0;
+		nuevoValorMaquina = 0;
+		nuevoValorCinta = 0;
+		direccion = 0;
+	}
 }
 
 
 class Parser
 {
+	public final static int charComodin = '*';
+	public final static int charFin = 'h';
+	public final static int charBlanco = 'b';
+
+
+	
 	public Transicion [] transiciones;
 	public int nuTransiciones;
+	public boolean hayTransiciones;
+	public boolean hayCinta;
+
 	
 	Parser(String filename)
 	{
-		transiciones = new Transicion[200];
-		nuTransiciones = 0;
+		this.hayTransiciones = true;
+		this.hayCinta = false;
+		
+		this.transiciones = new Transicion[200];
+		for(int i = 0; i < 200; ++i)
+			this.transiciones[i] = new Transicion();
+		
+		this.nuTransiciones = 0;
 		
 		
-		
-		 try{
-			  FileInputStream fstream = new FileInputStream(filename);
-			  DataInputStream in = new DataInputStream(fstream);
-			  BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			  
-			  String strLine;
-			  while ((strLine = br.readLine()) != null)
-			  {  
-				  
-				  Transicion t = transiciones[nuTransiciones];
-				  if(parseLine(strLine, t))
-					  print(t);
-				  else {
-					  System.out.println("Error reading line "+nuTransiciones+1);
-					  return;
-				  }  
-				  ++nuTransiciones;
-			  }
-			  
-			  //Close the input stream
-			  in.close();
-			  
+		readFile(filename);
+	}
+	
+
+	protected boolean readFile(String filename)
+	{
+		System.out.println("Parsing \""+filename+"\"");
+
+		try{
+			Scanner sc = new Scanner(new File(filename));
+
+			while (sc.hasNextLine()) {
+				
+				String strLine = sc.nextLine();
+				Transicion t = this.transiciones[this.nuTransiciones];
+				
+				if(parseLine(strLine, t))
+					print(t);
+				else {
+					System.out.println("Error reading line "+(this.nuTransiciones+1));
+					return false;
+				}
+				++this.nuTransiciones;
+			}
+			
 		}catch (Exception e){ //Catch exception if any
-			  System.err.println("Error: " + e.getMessage());
+			System.err.println("Error: " + e.getMessage());
+			return false;
+		}
+		return true;
+	}
+	
+	
+	protected int parseZone(String subZone)
+	{
+		char first = subZone.charAt(0);
+		switch(first) {
+		case charComodin:
+			return MaquinaTuring.valorComodin;
+			
+		case charFin:
+			return MaquinaTuring.valorFin;
+			
+		case charBlanco:
+			return MaquinaTuring.valorBlanco;
+			
+		default:
+			return Integer.parseInt(subZone);
 		}
 	}
 	
 	
 	protected boolean parseLine(String line, Transicion t)
-	{
-		String[] zonas = line.split(" ");
-		
+	{		
+		String[] zonas = line.split("\\s+");
+
 		if(zonas.length < 5)
 			return false;
 		
-		t.valorMaquina = Integer.parseInt(zonas[0]);
-		if(t.valorMaquina < 0 || t.valorMaquina > 6)
-			return false;
+		t.valorMaquina = parseZone(zonas[0]);
+		t.valorCinta = parseZone(zonas[1]);
+		t.nuevoValorMaquina = parseZone(zonas[2]);
+		t.nuevoValorCinta = parseZone(zonas[3]);
+		t.direccion = parseZone(zonas[4]);
 		
-		t.valorCinta = Integer.parseInt(zonas[1]);
-		if(t.valorCinta < 0 || t.valorCinta > 1)
-			return false;
-		
-		t.nuevoValorMaquina = Integer.parseInt(zonas[2]);
-		if(t.nuevoValorMaquina < 0 || t.nuevoValorMaquina > 5)
-			return false;
-		
-		t.nuevoValorCinta = Integer.parseInt(zonas[3]);
-		if(t.nuevoValorCinta < 0 || t.nuevoValorCinta > 6)
-			return false;
-		
-		t.direccion = Integer.parseInt(zonas[4]);
-		if(t.direccion != -1 || t.direccion != 1)
-			return false;
-
 		return true;
 	}
 	
+	
 	protected void print(Transicion t)
 	{
-		System.out.printf("%d %d %d %d %d",
+		System.out.printf("%d %d %d %d %d \n",
 				t.valorMaquina,
 				t.valorCinta,
 				t.nuevoValorMaquina,
